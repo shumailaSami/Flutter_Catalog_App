55e981beb87e593035b13330a1940efa176cf740
Shumaila <shumaila90.sami@gmail.com>
2021-12-27 16:32:05 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
-
 import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
 
-
-class  MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-          
+    return MaterialApp(
+      home: HomePage(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
+
+  final int days = 30;
+  final String name = "Coder";
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter $name"),
+        ),
+      ),
+      drawer: Drawer(),
     );
   }
 }
