fa03ab18c48f35e6162e30819c9ee28be00ccc2d
Shumaila <shumaila90.sami@gmail.com>
2022-01-08 16:54:01 +0530

    day12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,18 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "iPhone 12 pro",
+        desc: "Apple iPhone 12th generation",
+        price: 999,
+        color: "#33505a",
+        image:
+            "http://tineye.com/images/widgets/mona.jpg")
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -14,14 +27,3 @@ class Item {
       required this.color,
       required this.image});
 }
-
-final products = [
-  Item(
-      id: "dev101",
-      name: "iPhone 12 pro",
-      desc: "Apple iPhone 12th generation",
-      price: 999,
-      color: "#33505a",
-      image:
-          "https://www.google.com/imgres?imgurl=https%3A%2F%2F5.imimg.com%2Fdata5%2FANDROID%2FDefault%2F2020%2F10%2FDA%2FGX%2FTA%2F115146420%2Fproduct-jpeg-500x500.jpg&imgrefurl=https%3A%2F%2Fwww.indiamart.com%2Fproddetail%2Fcomfy-graceful-men-shirts-22805523655.html&tbnid=qSrhjyTputfXmM&vet=12ahUKEwjioLqB3aH1AhWyQ3wKHd3jDpEQMygBegUIARCLAg..i&docid=1najkETjm3irfM&w=500&h=500&itg=1&q=shirt%20for%20men&ved=2ahUKEwjioLqB3aH1AhWyQ3wKHd3jDpEQMygBegUIARCLAg")
-];
