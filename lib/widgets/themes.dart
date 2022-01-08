43a6db01c755ac12eba2aae72be0a5f60cdb2e2f
Shumaila <shumaila90.sami@gmail.com>
2022-01-08 15:50:15 +0530

    day11 completed

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme {
+  
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
       fontFamily: GoogleFonts.lato().fontFamily,
